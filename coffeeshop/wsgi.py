07662d9c8a6d30cd0ce4c1e1cefb396a04959093
manasa <manasarasur14@gmail.com>
2025-09-21 16:11:50 +0530

    Update CoffeeShop code

--- coffeeshop/wsgi.py
+++ coffeeshop/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coffeeshop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CoffeeShop.settings')
 
 application = get_wsgi_application()
